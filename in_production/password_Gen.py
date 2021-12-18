45a1a040074a1b84a9306126f3d2239ddd6be087
Mark <m.minchoff@gmail.com>
2021-12-17 19:06:43 -0500

    created a password generator

---
+++ in_production/password_Gen.py
@@ -0,0 +1,29 @@
+#password_gen
+
+#import mods
+import string
+import random
+
+#welcome message
+print('Welcome to a "Password Generator"!')
+
+#user inputs the length of password
+#\n creates a new line
+length = int(input('\nEnter the length of the password: '))
+
+#defining data
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+
+#combine the data
+all = lower + upper + num + symbols
+
+#random module
+temp = random.sample(all, length)
+
+password = "".join(temp)
+
+#print the password
+print(password)
